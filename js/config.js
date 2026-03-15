3b89d6323ad443d282eff47395b9ae41275612d5
Kengo T <kengo.nd77@gmail.com>
2026-03-14 23:04:06 -0500

    test_1

--- js/config.js
+++ js/config.js
@@ -2,8 +2,9 @@
   const SUPABASE_URL = 'http://127.0.0.1:54321';
   const SUPABASE_ANON_KEY = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH';
   const AUDIO_BASE_URL = 'https://blackstraysheep.github.io/khb_onlinesheet/audio/';
+  //const AUDIO_BASE_URL = '../audio/';
   const TOKEN_PREFIX = 'khb-';
-  const TOKEN_LENGTH = 32;
+  const TOKEN_LENGTH = 8;
 
   const appConfig = Object.freeze({
     SUPABASE_URL,
